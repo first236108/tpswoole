b45763e40de9cf2ce1152d7229114d831c8af556
first236108 <85210755@qq.com>
2018-07-07 11:55:14 +0800

    add the swoole composer

---
+++ config/swoole.php
@@ -0,0 +1,31 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+use think\facade\Env;
+
+// +----------------------------------------------------------------------
+// | Swoole设置 php think swoole命令行下有效
+// +----------------------------------------------------------------------
+return [
+    // 扩展自身配置
+    'host'     => 'igccc.com', // 监听地址
+    'port'     => 9503, // 监听端口
+    'work_num' => 4,
+    'max_request'=> 1000,
+    'max_conn' => 5000,
+    'task_worker_num'=>200,
+    'app_path' => '/www/tpswoole', // 应用地址 如果开启了 'daemonize'=>true 必须设置（使用绝对路径）
+
+    // 可以支持swoole的所有配置参数
+    'pid_file' => Env::get('runtime_path') . 'swoole.pid',
+    'log_file' => Env::get('runtime_path') . 'swoole.log',
+    'daemonize'=> true,
+];
