943f325aaba4dd166db686eb76362cfee040f3ad
潘志强 <85210755@qq.com>
2018-07-07 12:24:36 +0800

    add the Swoole.php of controller

---
+++ application/index/controller/Swoole.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 85210755@qq.com
+ * NickName: 柏宇娜
+ * Date: 2018/7/7 12:14
+ */
+
+namespace app\index\controller;
+
+use think\swoole\Server;
+
+class Swoole extends Server
+{
+    protected $host = 'igccc.com';
+    protected $port = 9502;
+    protected $option = [
+        'worker_num'      => 4,
+        'max_request'     => 1000,
+        'max_conn'        => 5000,
+        'task_worker_num' => 200,
+        'backlog'         => 128,
+        'daemonize'       => true,
+    ];
+
+    public function onReceive($server, $fd, $from_id, $data)
+    {
+        echo "onReceive...".PHP_EOL;
+        $server->send($fd, 'Swoole: ' . $data);
+    }
+
+    public function onOpen($server, $fd, $from_id, $data)
+    {
+        echo "onOpen...".PHP_EOL;
+        $server->send($fd, 'Swoole: ' . $data);
+    }
+
+    public function onMessage($server, $fd, $from_id, $data)
+    {
+        echo "onMessage...".PHP_EOL;
+        $server->send($fd, 'Swoole: ' . $data);
+    }
+
+    public function onClose($server, $fd, $from_id, $data)
+    {
+        echo "onClose...".PHP_EOL;
+        $server->send($fd, 'Swoole: ' . $data);
+    }
+}
--- config/swoole.php
+++ config/swoole.php
@@ -16,16 +16,13 @@ use think\facade\Env;
 // +----------------------------------------------------------------------
 return [
     // 扩展自身配置
-    'host'     => 'igccc.com', // 监听地址
-    'port'     => 9503, // 监听端口
-    'work_num' => 4,
-    'max_request'=> 1000,
-    'max_conn' => 5000,
-    'task_worker_num'=>200,
-    'app_path' => '/www/tpswoole', // 应用地址 如果开启了 'daemonize'=>true 必须设置（使用绝对路径）
+    'host'            => 'igccc.com', // 监听地址
+    'port'            => 9503, // 监听端口
+    'work_num'        => 4,
+    'app_path'        => '/www/tpswoole', // 应用地址 如果开启了 'daemonize'=>true 必须设置（使用绝对路径）
 
     // 可以支持swoole的所有配置参数
-    'pid_file' => Env::get('runtime_path') . 'swoole.pid',
-    'log_file' => Env::get('runtime_path') . 'swoole.log',
-    'daemonize'=> true,
+    'pid_file'        => Env::get('runtime_path') . 'swoole.pid',
+    'log_file'        => Env::get('runtime_path') . 'swoole.log',
+    'daemonize'       => true,
 ];
